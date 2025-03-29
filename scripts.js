ff956cef190bcb8586d4a48e78f15199b722130e
supergamehub <supergamehub1@gmail.com>
2025-03-29 13:15:52 +0100

    .

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const gameLinks = document.querySelectorAll(".listing .column a");
     const gameFrame = document.getElementById("game-frame");
-    const contentArea = document.querySelector(".g-content");
 
     gameLinks.forEach(link => {
         link.addEventListener("click", (event) => {
@@ -12,10 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function loadGame(url) {
-        // Clear the content area and show the iframe
-        contentArea.innerHTML = '';
         gameFrame.src = url;
         gameFrame.style.display = "block";
-        contentArea.appendChild(gameFrame);
     }
 });
