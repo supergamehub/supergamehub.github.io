164dd81e4e657e8c98c4affae24d1c116341c518
supergamehub <supergamehub1@gmail.com>
2025-03-29 16:57:40 +0100

    .

--- scripts.js
+++ scripts.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const games = [
+        { name: "Madness Cars Destroy", category: "accion", url: "https://html5.gamedistribution.com/2f5c17b7d11f449c9224f09041d1c312/?timestamp=-62135596800&countryCode=es&siteid=88&channelid=1&siteLocale=es&locale=es&gd_sdk_referrer_url=https%3A%2F%2Fwww.juegos.com%2Fjuego%2Fmadness-cars-destroy", img: "https://img.gamedistribution.com/2f5c17b7d11f449c9224f09041d1c312-512x512.jpg" },
         { name: "Mystical Forest", category: "estrategia", url: "https://cdn.htmlgames.com/MysticalForest/", img: "https://www.htmlgames.com/uploaded/game/thumb200/mysticalforest200.webp" },
         { name: "Sudoku Classic", category: "rompecabezas", url: "https://cdn.htmlgames.com/SudokuClassic/", img: "https://www.htmlgames.com/uploaded/game/thumb200/sudokuclassic200.webp" },
         { name: "Candy Mahjong", category: "rompecabezas", url: "https://cdn.htmlgames.com/CandyMahjong/", img: "https://www.htmlgames.com/uploaded/game/thumb200/candymahjong200.webp" },
